6baf387347f6da247d47d48c0ce77eb14a4ee0ca
Fabricio Castro <baptistafabricio87@gmail.com>
2023-06-09 16:10:18 -0300

    Configuracao de urls e apps

--- setup/urls.py
+++ setup/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls'), name='core'),
-    path('accounts/', include('accounts.urls'), name='accounts')
+    path('accounts/', include('accounts.urls'), name='accounts'),
+    path('salas/', include('salas.urls'), name='salas')
 ]
